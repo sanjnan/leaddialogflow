19a5c5181d328e69d7dcb40c2e963e48284fb094
sanjnan <34151371+sanjnan@users.noreply.github.com>
2018-05-10 15:19:41 +0530

    Update script.py

--- script.py
+++ script.py
@@ -77,24 +77,24 @@ def posti():
                                                 else:
                                                     respo = {"fulfillmentText": "Please enter a valid 10 digit mobile","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {}}
                                                     return jsonify(respo)
-                            elif req[header][item][option] == "otp":
+                            elif req[header][item][option] == "askotp":
                                 for itm in req[header]:
                                     if itm == "parameters":
                                         for para in req[header][itm]:
-                                            if para == "otpno":
+                                            if para == "otp":
                                                 p = re.compile(r'^\d{6}$',re.I|re.M)
                                                 print (req[header][itm][para])
                                                 if p.match(str(req[header][itm][para])):
-                                                    respo = {"fulfillmentText": "Do you wanna apply loan or raise a service request? Please type","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {}}
+                                                    respo = {"fulfillmentText": "what are you looking for ? Apply Loan,Track Your Loan Status,Customer Service,Track Your SR Status","fulfillmentMessages": [{"card":{"buttons": [{"text": "Apply Loan", "postback": "https://assistant.google.com/"}]}}],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {}}
                                                     return jsonify(respo)
                                                 else:
                                                     respo = {"fulfillmentText": "Please enter a valid 6 digits OTP sent to your mobile number","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {}}
                                                     return jsonify(respo)               
-                            respo = {"fulfillmentText": "OOPS sorry!Please chat sometime later","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {}}
+                            respo = {"fulfillmentText": "This is not greeting intent","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {}}
                             return jsonify(respo)
     #session = request.json["session"]
     #querytext = request.json["querytext"]
-    #mobnum = request.json["mobnum"]
+    #mobilenumber = request.json["mobilenumber"]
 
 
 @app.route('/about/')
